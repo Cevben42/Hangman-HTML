a0454cc8d86012901d77c1151922420270a024d6
Carl <143086707+Cevben42@users.noreply.github.com>
2025-07-03 23:22:26 +0800

    Add files via upload
    Added the folders

---
+++ Hangman/script.js
@@ -0,0 +1,85 @@
+//list of words to be used. Update if you can
+const wordList = [
+    "metal", 
+    "Iron", 
+    "Mercury", 
+    "Puzzle", 
+    "Hangman", 
+    "Uranium", 
+    "Fever", 
+    "Thermodynamics", 
+    "Ceiling", 
+    "Ten"
+    "Jollibee"
+];
+
+let selectWord="";
+let correctLetters=[];
+let wrongLetters=[];
+
+const wordDisplay=
+document.getElementById("wordDisplay");
+const wrongLettersE1=
+document.getElementById("wrongLetters");
+const input=
+document.getElementById("letterInput");
+const message=
+document.getElementById("message");
+const startBTn=
+document.getElementById("startBtn");
+
+function updateDisplay() {
+    const display=selectedWord
+        .split("")
+        .map(letter => (correctLetters.includes(letter) ? letter: "_"))
+        .join(" ");
+        wordDisplay.textContent=display;
+
+        if (!display.includes("_")) {
+            message.textContent="You Won!! >O<";
+            input.disabled=true;
+        }
+}
+
+function checkLose() {
+    if (wrongLetters.length >= 6) {
+        message.textContent = `You lost >_<. The word was "${selectedWord}"`;
+        input.disabled=true;
+    }
+}
+
+input.addEventListener("keyup", e => {
+    const letter=
+    e.target.value.toLowerCase();
+    if (!letter.match(/^[a-z]$/i)) return;
+
+    if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+            correctLetters.push(letter);
+            updateDisplay();
+        }
+    } else {
+        if (!wrongLetters.includes(letter)) {
+            wrongLetters.push(letter);
+            wrongLettersE1.textContent=wrongLetters.join(", ");
+            checkLose();
+        }
+    }
+
+    e.target.value="";
+});
+
+startBtn.addEventListener("click", () =>
+{
+    selectedWord=wordList[Math.floor(Math.random() * wordList.length)].toLowerCase();
+    correctLetters=[];
+    wrongLetters=[];
+
+    wordDisplay.textContent="_".repeat(selectedWord.length).trim();
+    wrongLettersE1.textContent="";
+    message.textContent="";
+    input.disabled=false;
+    input.focus();
+    input.value="";
+}
+);
