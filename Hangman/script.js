a0eebf97a689f74ad5745b7d19762f3732bdd654
Carl <143086707+Cevben42@users.noreply.github.com>
2025-07-07 21:13:22 +0800

    Update script.js

--- Hangman/script.js
+++ Hangman/script.js
@@ -21,7 +21,11 @@ const wordList = [
     "Gourd",
     "Creepjoiner",
     "Yttakin",
-    "Exterminatus"
+    "Exterminatus",
+    "Cinnamon",
+    "Cupcake",
+    "Baguette",
+    "Cocoa"
 ];
 
 let selectWord="";
