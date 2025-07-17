4ec742785323baa3ae394564333ac0729a6b9273
Carl <143086707+Cevben42@users.noreply.github.com>
2025-07-17 21:42:43 +0800

    Update script.js

--- Hangman/script.js
+++ Hangman/script.js
@@ -50,7 +50,11 @@ const wordList = [
     "Duke",
     "Confederation",
     "Diplomacy",
-    "Advance"
+    "Advance",
+    "Change",
+    "Large",
+    "Colossal",
+    "Pomegranate"
 ];
 
 let selectWord="";
